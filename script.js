8fbb05e6c36ede683ef6f1a08b6b45564d99df1b
mariaritabittencourtdossantos-sketch <maria.ritabittencourtdos.santos@escola.pr.gov.br>
2026-08-31 18:24:47 -0300

    s

---
+++ script.js
@@ -0,0 +1,9 @@
+botoes.forEach(function(botao) {
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        texto.textContent++;
+    }
+});
